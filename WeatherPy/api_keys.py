c679eef2cb8cda9e9dc5a660b989ede04060d8fc
Peter Nguyen <nguyen.peter24@gmail.com>
2020-05-07 12:34:05 -0700

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bd612ae7ba586cb45f4ed29071cacabb"
 
 # Google API Key
 g_key = "AIzaSyBoZlgz_ib2ivWx9sSmF-u94od3kJYhuUI"
